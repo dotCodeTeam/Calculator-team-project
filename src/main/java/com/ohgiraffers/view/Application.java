0c9df99316b01927e3de79d03b35fffbd4d95657
soobin <nunu1101@gmail.com>
2024-02-21 09:47:26 +0900

    윤수빈 더하기 기능 추가 및 Application 추가

--- src/main/java/com/ohgiraffers/view/Application.java
+++ src/main/java/com/ohgiraffers/view/Application.java
@@ -1,4 +1,14 @@
 package com.ohgiraffers.view;
 
+import com.ohgiraffers.run.Caculator;
+
+import java.util.Scanner;
+
 public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Caculator cala = new Caculator();
+        int a1, b1;
+
+    }
 }
